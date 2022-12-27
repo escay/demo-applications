a7221bc4c2d5e19e390b7b8635e47efe8e481184
R.M.Morrien <dj.escay@gmail.com>
2022-12-27 16:11:55 +0100

    Add pure SQL example set set connection pool limit in application.properties

---
+++ quarkus.hibernate.stateless.session/src/main/java/nl/escay/DirectDatabaseService.java
@@ -0,0 +1,69 @@
+package nl.escay;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.agroal.api.AgroalDataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import nl.escay.entity.PersonRecord;
+
+@ApplicationScoped
+public class DirectDatabaseService {
+
+    @Inject
+    AgroalDataSource defaultDataSource;
+
+    @Transactional
+    public void createPersonUsingJdbcSql(Long id, String firstName, String lastName)
+            throws SQLException {
+
+        try (Connection connection = defaultDataSource.getConnection();
+                PreparedStatement prepareStatement = connection.prepareStatement(
+                        "INSERT INTO Person (id, firstName, lastName) VALUES (?, ?, ?)");) {
+            // Disadvantage: can only use index, no named references to ":id" like in Hibernate or JOOQ.
+            // Disadvantage: no sql logging documentation. No parameter logging like in Hibernate 6.
+            prepareStatement.setInt(1, id.intValue());
+            prepareStatement.setString(2, firstName);
+            prepareStatement.setString(3, lastName);
+            prepareStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    public List<PersonRecord> getPersonsAsRecordUsingJdbcSql() throws SQLException {
+        List<PersonRecord> personRecords = new ArrayList<>();
+        
+        try (Connection connection = defaultDataSource.getConnection();
+                PreparedStatement prepareStatement = connection.prepareStatement(
+                    "select id, firstName, lastName from Person");
+                ResultSet rs = prepareStatement.executeQuery()) {
+            
+            // Disadvantage: cannot use stream. Must use ResultSet
+            // jOOQ has made a nice comparison: https://www.jooq.org/java-8-and-sql
+            // Disadvantage: Unluckily we must apply the wasNull overhead code from the old days.
+            // Disadvantage: no sql logging documentation. No parameter logging like in Hibernate 6.
+            while (rs.next()) {
+                Integer id = rs.getInt("id");
+                if (rs.wasNull()) id = null;
+                String firstName = rs.getString("firstName");
+                if (rs.wasNull()) firstName = null;
+                String lastName = rs.getString("lastName");
+                if (rs.wasNull()) lastName = null;
+                
+                personRecords.add(new PersonRecord(id, firstName, lastName));
+            }
+        }
+        
+        System.out.println(Arrays.toString(personRecords.toArray()));
+        return personRecords;
+    }
+    
+}
--- quarkus.hibernate.stateless.session/src/test/java/nl/escay/PersonServiceTest.java
+++ quarkus.hibernate.stateless.session/src/test/java/nl/escay/PersonServiceTest.java
@@ -17,8 +17,11 @@ public class PersonServiceTest {
     @Inject
     PersonService service;
 
+    @Inject
+    DirectDatabaseService ddService;
+    
     @Test
-    public void testJpaNativeQuery() {
+    public void testJpaNativeQuery() throws Exception {
         // Insert a record using the entity manager
         service.createPerson(5L, "Tom", "Erichsen");
         service.createPerson(8L, "Ben", "Johanssen");
@@ -29,16 +32,24 @@ public class PersonServiceTest {
         service.createPersonUsingSqlAndStatelessSesssion(13L, "Harmony ", "Hill");
         service.createPersonUsingSqlAndStatelessSesssion(14L, "Aleeza ", "Prince");
         
+        // Insert some records without using no HIbernate / JPA only JDBC
+        ddService.createPersonUsingJdbcSql(15L, "Hayley", "Hahn");
+        ddService.createPersonUsingJdbcSql(16L, "Fabian", "Dean");
+        
         // Query the database content using a native query and no transaction
         List<PersonDTO> personsAsDtoUsingTuple = service.getPersonsAsDtoUsingTuple();
-        assertEquals(6, personsAsDtoUsingTuple.size());
+        assertEquals(8, personsAsDtoUsingTuple.size());
 
         // Query the database content using a native query and no transaction
         List<PersonRecord> personsAsRecordUsingTuple = service.getPersonsAsRecordUsingTuple();
-        assertEquals(6, personsAsRecordUsingTuple.size());
+        assertEquals(8, personsAsRecordUsingTuple.size());
 
         // Query the database content using a StatelessSession and a native query and no transaction
         List<PersonRecord> personsAsRecordUsingTupleAndStatelessSession = service.getPersonsAsRecordUsingTupleAndStatelessSession();
-        assertEquals(6, personsAsRecordUsingTupleAndStatelessSession.size());
+        assertEquals(8, personsAsRecordUsingTupleAndStatelessSession.size());
+        
+        // Query the database content using a Jdbc Sql only
+        List<PersonRecord> personsAsRecordUsingJdbcSql = ddService.getPersonsAsRecordUsingJdbcSql();
+        assertEquals(8, personsAsRecordUsingJdbcSql.size());
     }
 }
